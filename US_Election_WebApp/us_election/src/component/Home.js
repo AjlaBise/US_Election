84b7b12bddcc57f36aea6ef79a9fa139dcc81f70
Ajla Bise <ajla.bise@edu.fit.ba>
2021-11-18 00:59:45 +0100

    Setup React projet

---
+++ US_Election_WebApp/us_election/src/component/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Presidential election results</p>
+      <div className="box">
+        <span>&bull; &bull; &bull;</span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
